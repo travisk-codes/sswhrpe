b58942466101232aebd3e9617cfea3e30b8bd5df
Travis Kohlbeck <me+git@travisk.info>
2020-04-06 14:58:16 -0400

    Send Twitch API data to the client - Connect client to server and display data on change - Add server file with socket and webhook configured - Add Twitch credentials and required client/server packages

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const http = require('http')
+const io = require('socket.io')
+const path = require('path')
+
+const TwitchClient = require('twitch').default
+const TwitchStream = require('twitch').HelixStream
+const WebhookListener = require('twitch-webhooks').default
+
+const { userId, clientId, secret } = require('./credentials')
+
+const app = express()
+const server = http.createServer(app)
+const socket = io(server, { origins: '*:*' })
+const twitchClient = TwitchClient.withClientCredentials(clientId, secret)
+
+const webhookConfig = {
+	hostName: 'c5c8942d.ngrok.io',
+	port: 3020,
+	reverseProxy: { port: 443, ssl: true },
+}
+
+const getWebhookSubscription = async () => {
+	const listener = await WebhookListener.create(twitchClient, webhookConfig)
+	listener.listen()
+	return await listener.subscribeToStreamChanges(
+		userId,
+		(stream = TwitchStream) => {
+			console.log('new data emitted:', stream.title)
+			socket.emit('newData', stream.title)
+		},
+	)
+}
+const subscription = getWebhookSubscription()
+
+socket.on('connection', () => {
+	console.log('websocket connection established with client')
+})
+
+app.get('/*', (req, res) => {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+server.listen(3010, () => console.log('listening on 3010'))
--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import socketIOClient from 'socket.io-client'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const socket = socketIOClient('http://localhost:3010')
+
+const App = () => {
+	const [state, setState] = React.useState()
+
+	React.useEffect(() => {
+		socket.on('connect', () =>
+			console.log('websocket connection established with server'),
+		)
+		socket.on('newData', (data) => setState(data))
+		return () => socket.off('newData')
+	})
+
+	return (
+		<div className='App'>
+			<h1>Simple Socket WebHook Reverse Proxy Example</h1>
+			You can find socket data in the console, or below:
+			<br />
+			<br />
+			{state}
+		</div>
+	)
 }
 
-export default App;
+export default App
